849f4343f7a98569a53f07a9b6a0f265dab6a691
FranciscoC <123523494+CarpinchoRecu@users.noreply.github.com>
2023-09-01 15:05:25 -0300

    servidor 1.0.0

--- src/routes/contacto.js
+++ src/routes/contacto.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const contactoController = require("../controllers/contactoController.js");
 
 // Ruta para manejar la solicitud de contacto
-router.post("/", contactoController.enviarMensaje);
+router.post("/contacto", contactoController.enviarMensaje);
 
 module.exports = router;
